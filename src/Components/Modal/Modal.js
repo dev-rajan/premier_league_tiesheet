463880e73269d6c05b3e1add03cef98489401d86
dev-rajan <rajansth77@gmail.com>
2022-12-17 21:16:00 +0545

    modal design and data update

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,33 +1,39 @@
 import React from "react";
 
+import PremierLogo from "../../assets/images/clubs/premier.png";
+
 const Modal = ({ handleCloseModal, modalData }) => {
   return (
     <div className="modal">
       <div className="modal__content">
         <div className="modal__title">
           <h1>
-            <span className="today__date">{modalData?.round}</span> &bull;{" "}
-            <span>{modalData?.date}</span>
+            <span>
+              <img src={PremierLogo} alt={modalData.name} />
+            </span>
+            <span className="today__date">{modalData.name} stats</span>
           </h1>
+          <button
+            type="button"
+            className="modal__close"
+            onClick={handleCloseModal}
+          >
+            &times;
+          </button>
         </div>
 
         <div className="modal__body">
-          <div className="team">{modalData?.team1}</div>
-          <div className="score">
-            <div className="team__score">{modalData?.score?.ft[0]}</div>
-            <span className="team__score">-</span>
-            <div className="team__score">{modalData?.score?.ft[1]}</div>
+          <div className="col">Played: {modalData.played}</div>
+          <div className="col">Won: {modalData.won}</div>
+          <div className="col">Draw: {modalData.draw}</div>
+          <div className="col">Lost: {modalData.lost}</div>
+          <div className="col">GF: {modalData.gf}</div>
+          <div className="col">GA: {modalData.ga}</div>
+          <div className="col">
+            GD: {modalData.gd > 0 ? `+${modalData.gd}` : modalData.gd}
           </div>
-          <div className="team">{modalData?.team2}</div>
+          <div className="col">Points: {modalData.points}</div>
         </div>
-
-        <button
-          type="button"
-          className="modal__close"
-          onClick={handleCloseModal}
-        >
-          &times;
-        </button>
       </div>
     </div>
   );
